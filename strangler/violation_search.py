f88508991c0f53eb8c7990ad1a09010b48abf895
michaelrmentele <michaelrmentele@gmail.com>
2019-07-28 19:55:04 -0700

    searches and filters for violations

--- strangler/violation_search.py
+++ strangler/violation_search.py
@@ -5,16 +5,38 @@ import re, os
 
 class ViolationSearch:
     @staticmethod
-    def find_module_violations(filepattern, module):
-        regexes = ViolationSearch._construct_regex_from_modules(module)
-        files = ViolationSearch._find_files(project_path, filepattern)
+    def find_module_violations(project_path, definition):
+        '''
+        Accepts a module name that we will search for import from, then
+        checks those module import patterns against all files that match
+        the file pattern.
+
+        This is then filtered down, where anything that matches what is
+        public for that module and is NOT explicitly private, is excluded.
+        Why is it excluded? Because it is a valid export.
+        '''
+        # Find all exports for this module
+        regexes = ViolationSearch._construct_regex_from_module(definition.module)
+        files = ViolationSearch._find_files(project_path, definition.file_pattern)
         violations = []
         for f in files:
-            violations.append(self._find_file_matches(f, regexes, project_root))
-        return violations
+            matches = ViolationSearch._find_file_matches(f, regexes, project_path)
+            if matches: violations.extend(matches)
+
+        # Filter down to actual violations:
+        # 1. is public
+        # 2. is not explicitly private
+        return [
+            violation for violation in violations
+            # It's a violation if it's not public AKA private
+            if not (
+                any([re.match(pattern, violation[0]) for pattern in definition.public]) and
+                not any([re.match(pattern, violation[0]) for pattern in definition.private])
+            )
+        ]
 
     @staticmethod
-    def _construct_regex_from_modules(module):
+    def _construct_regex_from_module(module):
         patterns = []
         patterns.append(re.compile('^from .*{}.*$'.format(module)))
         patterns.append(re.compile('^import .*{}.*$'.format(module)))
@@ -26,7 +48,10 @@ class ViolationSearch:
         Given a set of patterns, find all files from a search
         path.
         '''
-        return list(Path(project_path).glob(unix_file_pattern))
+        return [
+            f for f in Path(project_path).glob(unix_file_pattern)
+            if f.is_file()
+        ]
 
     @staticmethod
     def _find_file_matches(f, regexes: List[str], project_root) -> list:
@@ -34,17 +59,13 @@ class ViolationSearch:
         Given a set of patterns and files find all the lines that
         match.
         '''
-        def relativize(path, relative_root):
-            prefix = os.path.commonprefix([path, relative_root])
-            # trim out the prefix from abs_path
-            # add 1 to account for the leading forward slash
-            return path[len(prefix) + 1:]
 
         def file_matches(f, regex) -> list:
             matches = []
             for lineno, line in enumerate(open(f)):
                 if re.match(regex, line):
-                    matches.append((relativize(os.path.abspath(f.name), project_root), lineno + 1, line))
+                    project_relative_path = f.absolute().relative_to(project_root).as_posix()
+                    matches.append((project_relative_path, lineno + 1, line))
             return matches
 
         matches = []
--- tests/test_violation_search.py
+++ tests/test_violation_search.py
@@ -2,9 +2,32 @@ from unittest import TestCase
 import os
 
 from strangler.violation_search import ViolationSearch
+from strangler.strangler import Interface
 
 
 class TestViolationSearch(TestCase):
+
+    def test_find_module_violations(self):
+        project_path = os.getcwd()
+
+        # Scenario: One way imports
+        with self.subTest():
+            interface = Interface({
+                'file_pattern': 'tests/test_data/one_way_imports/**/*.py',
+                'module': 'extensionBar',
+                # not defining public, implicitly means nothing is public
+            })
+            # Since nothing is public we should throw errors if we export
+            # and with this example we are exporting
+            violations = ViolationSearch.find_module_violations(project_path, interface)
+            self.assertEqual(len(violations), 1)
+            self.assertEqual(
+                violations,
+                [('tests/test_data/one_way_imports/extensionFoo.py', 2, 'from extensionBar import stuff\n')]
+            )
+
+        # Scenario: Cross module imports
+
     def test_find_files(self):
         self.assertEqual(
             [f.name for f in ViolationSearch._find_files('', '**/test_violation_search.py')],
@@ -18,7 +41,7 @@ class TestViolationSearch(TestCase):
         self.assertEqual(
             ViolationSearch._find_file_matches(f, patterns, os.getcwd()),
             [
-                (filename, 1, 'from typing import List\n'),
-                (filename, 2, 'from pathlib import Path\n'),
+                ('strangler/' + filename, 1, 'from typing import List\n'),
+                ('strangler/' + filename, 2, 'from pathlib import Path\n'),
             ]
         )
